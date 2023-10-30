f67e0be1dbc04c4e1bce6b18ec029678ff6ee23c
jackbellinger <5804016+jackbellinger@users.noreply.github.com>
2023-10-30 14:06:54 -0700

    Download font & symlink instead of copy rc's

--- dev-setup.sh
+++ dev-setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+set -x
 sudo apt upgrade && sudo apt -y update
 
 #General
@@ -5,16 +7,21 @@ sudo apt install -y git net-tools rsync
 
 #Shell
 sudo apt install -y zsh
+#get nerdfont patched mononoki
+curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Mononoki.tar.xz
+tar -xf Mononoki.tar.xz -C ~/.local/share/fonts
+rm Mononoki.tar.xz
+
 #install oh my zsh
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 #install powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+local dotfiles_dir = $(pwd)
 #copy zsh and p10k configs
-cp ./.zshrc ~/
-cp ./.p10k.zsh ~/
-cp ./.vimrc ~/
-cp ./.gitconfig ~/
-git config core.filemode true
+ln -s $dotfiles_dir/.zshrc ~/.zshrc
+ln -s $dotfiles_dir/.vimrc ~/.vimrc
+ln -s $dotfiles_dir/.gitconfig ~/.gitconfig
+ln -s $dotfiles_dir/.p10k.zsh ~/.p10k.zsh
 source ~/.zshrc
 
 #Images
@@ -30,6 +37,7 @@ pip3 install matplotlib
 
 #Rust
 curl https://sh.rustup.rs -sSf | sh -s -- -y
+reset
 
 #Webdev
 sudo apt install -y nodejs npm
