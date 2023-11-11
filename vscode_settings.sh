8d71358315fc01ddcff3ae8c14f4031b7171f337
jackbellinger <5804016+jackbellinger@users.noreply.github.com>
2023-11-10 19:25:03 -0800

    Add vscode keybinds and snippets

---
+++ vscode_settings.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cp ~/dotfiles/settings.json /mnt/c/Users/User/AppData/Roaming/Code/User/
+cp ~/dotfiles/snippets.code-snippets /mnt/c/Users/User/AppData/Roaming/Code/User/snippets/
+cp ~/dotfiles/keybindings.json /mnt/c/Users/User/AppData/Roaming/Code/User/
